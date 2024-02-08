8036e74323b7628e4c628178f36056f1af3e7e05
anandspaces <anandvaibhaw000@gmail.com>
2024-02-09 01:38:01 +0530

    template inheritance

--- dishes/urls.py
+++ dishes/urls.py
@@ -3,7 +3,7 @@ from .views import home,web,inform,confirm
 
 urlpatterns = [
     path('',home,name='home'),
-    path('restaurantweb/',web,name='web'),
+    path('web/',web,name='web'),
     path('inform/',inform,name='inform'),
     path('confirm/',confirm,name='confirm'),
 ]
