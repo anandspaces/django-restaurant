afe1478788b09b50de13c9113341d88b84dff4bd
anandspaces <anandvaibhaw000@gmail.com>
2024-05-15 18:27:10 +0530

    vercel config3

--- fullstack_restaurant/settings.py
+++ fullstack_restaurant/settings.py
@@ -75,23 +75,12 @@ WSGI_APPLICATION = 'fullstack_restaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "restaurantdb",
-        "USER": "root",
-        "PASSWORD": "150167",
-        "HOST": "",
-        "PORT": "3306",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
